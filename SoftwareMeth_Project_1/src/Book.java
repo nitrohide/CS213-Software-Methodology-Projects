ad03acdacd19664405ade627ac045b61aa007d75
AnurajDubey <anurajd2000@gmail.com>
2021-02-05 20:17:23 -0500

    Update Book.java

--- SoftwareMeth_Project_1/src/Book.java
+++ SoftwareMeth_Project_1/src/Book.java
@@ -5,13 +5,45 @@ public class Book {
     private String name;
     private boolean checkedOut;
     private Date datePublished;
+    
+
+    //Getter methods
+    public String getNumber() {
+        return number;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean getCheckedout() {
+        return checkedOut; 
+    }
+    public Date getDatePublished() {
+        return datePublished;
+    }
+
+
+    //Setter methods
+    public void setNumber(String newNumber) {
+        this.number = newNumber; 
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCheckedOut(boolean checkedOut) {
+        this.checkedOut = checkedOut;
+    }
+    public void setDatePublished(Date datePublished) {
+        this.datePublished = datePublished;
+    }
+
+
 
     @Override
-    public boolean equals(Object obj){
-
+    public boolean equals(Object obj){ 
+        
     }
     @Override
-    public String toString() {
+    public String toString() { 
 
     }
 }
