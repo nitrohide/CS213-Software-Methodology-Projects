0f4f1bd226490fb4a01cc1736bd9e740e6c332d1
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-08 13:17:34 -0500

    Update Library.java

--- SoftwareMeth_Project_1/src/Library.java
+++ SoftwareMeth_Project_1/src/Library.java
@@ -1,6 +1,10 @@
 public class Library {
     private Book[] books; // array-based implementation of the bag data structure private int numBooks; // the number of books currently in the bag
+    private int numBooks;
+
     public Library() { //default constructor to create an empty bag
+        numBooks=0;
+        books=new Book[4];
 
     }
     private int find(Book book) {// helper method to find a book in the bag private void grow() { } // helper method to grow the capacity by 4
@@ -24,4 +28,16 @@ public class Library {
     public void printByDate() {
 
     } //print the list of books by datePublished (ascending) public void printByNumber() { } //print the list of books by number (ascending) }
+    public Book search(int number){
+        for(int i=0;i<numBooks;i++){
+            if(books[i].getSerial()==number){
+                return books[i];
+            }
+        }
+        return null;
+    }
+
+    public int getNumBooks(){
+        return numBooks;
+    }
 }
