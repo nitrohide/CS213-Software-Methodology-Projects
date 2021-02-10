d64ff754ed85aaa7544dcbf7507fac568a9ac0a8
AnurajDubey <anurajd2000@gmail.com>
2021-02-09 22:02:45 -0500

    Update Library.java

--- SoftwareMeth_Project_1/src/Library.java
+++ SoftwareMeth_Project_1/src/Library.java
@@ -1,7 +1,19 @@
+/**
+ * The Library class is responsible for creating and maintaining a "bag", or array, of type Book. 
+ * It holds multiple methods that revolve around maintaining and manipulating this bag in order to serve as an effective library.
+ * @author Anuraj Dubey, Chenghao Lin 
+ */
+
+
 public class Library {
+
+
     private Book[] books; // array-based implementation of the bag data structure private int numBooks; // the number of books currently in the bag
     private int numBooks;
 
+    /**
+     * This is a contructor to create an initial empty bag of type Book. 
+     */
     public Library() { //default constructor to create an empty bag
         books = new Book[4];
     }
@@ -10,6 +22,11 @@ public class Library {
     public static final int BAG_GROW_VALUE = 4;
     public static final int NOT_FOUND = -1;
 
+    /**
+     * This is a method to find a certain book in the bag, given and input of type Book. 
+     * @param book 
+     * @return i, the index of the book being searched for.  
+     */
     private int find(Book book) { // helper method to find a book in the bag
 
         for (int i = 0; i < books.length; i++){
@@ -20,6 +37,11 @@ public class Library {
         return NOT_FOUND; //book not found
     }
 
+
+    /**
+     * This is a method to grow the bag by a given value of 4, anytime the bag gets overfilled by books. 
+     * It creates a new bag and copies the old bag into this new one. 
+     */
     private void grow() {  // helper method to grow the capacity by 4
 
         Book[] temp = new Book[books.length + BAG_GROW_VALUE]; //temp to hold all books before copying into new bag
@@ -29,7 +51,10 @@ public class Library {
         books=temp;
     }
 
-
+    /**
+     * This method adds a given inputted book at the end of the bag.
+     * @param book 
+     */
     public void add(Book book) { //adds book to end of array. If full, calls grow()
         if (books[books.length-1]!= null){
             grow();
@@ -43,6 +68,14 @@ public class Library {
         }
     }
 
+
+    /**
+     * This method removes a given book from the bag by setting that respective index to null.
+     * Additionally, it shifts books further in the bag to the left following the removal. 
+     * This readjustment is to make sure there is no empty spot in the middle of the bag. 
+     * @param book
+     * @return true if the removal was successful, or false if the removal was unsuccessful. 
+     */
     public boolean remove(Book book) { //also shifts and readjusts all interior elements after removal
         int index_to_remove = find(book);
 
@@ -63,6 +96,11 @@ public class Library {
         return false; //also displays "Unable to remove, the library does not have this book."
     }
 
+    /**
+     * This method checks out a book by setting the book's "checkedOut" value to true.
+     * @param book
+     * @return true if check out was successful, or false if it was unsuccessful. 
+     */
     public boolean checkOut(Book book) {
 
         int index_to_find = find(book);
@@ -81,6 +119,12 @@ public class Library {
         return false; //return false if checkout failed for any other reason
     }
 
+
+    /**
+     * This method returns a checked out book by setting its checkedOut() value to false. 
+     * @param book
+     * @return true if return was successful, or false if return was unsuccessful. 
+     */
     public boolean returns(Book book) {
 
         int index_to_find = find(book);
@@ -103,7 +147,9 @@ public class Library {
 
 
 
-
+    /**
+     * This method prints out all books in the bag in a sequence. 
+     */
     public void print() {  //print the list of books in the bag
 
         for(int i = 0; i < books.length; i++){
@@ -114,6 +160,10 @@ public class Library {
 
     }
 
+    /**
+     * This method prints out all books in the bag, by ascending order of the published dates.
+     * It uses a selection sort algorithm to sort all the books in the bag. 
+     */
     public void printByDate() { //print the list of books by datePublished (ascending)
 
 
@@ -161,7 +211,10 @@ public class Library {
 
     }
 
-
+    /**
+     * This method prints out all books in the bag, by ascending order of the serial numbers.
+     * It uses a selection sort algorithm to sort all the books in the bag. 
+     */
     public void printByNumber() { //print the list of books by number (ascending) }
 
 
@@ -188,16 +241,24 @@ public class Library {
         print(); //print the sorted bag
     }
 
-
+    /**
+     * This method is used to search for a book in the bag with an inputted serial/book number for convenience if needed. 
+     * @param number
+     * @return this returns the book object being searched for, or returns null if not found.
+     */
     public Book search(int number){
         for (int i = 0;i < numBooks; i++){
-            if (Integer.parseInt(books[i].getSerial())==number){
+            if (Integer.parseInt(books[i].getSerial()) == number){
                 return books[i];
             }
         }
         return null;
     }
 
+    /**
+     * This method returns the number of books in the bag if needed. 
+     * @return returns the number of books. 
+     */
     public int getNumBooks(){
         return numBooks;
     }
