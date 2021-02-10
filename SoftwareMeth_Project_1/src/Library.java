9f431d90619bcea21409d58ad707efd04061a483
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-09 22:33:35 -0500

    Update Library.java

--- SoftwareMeth_Project_1/src/Library.java
+++ SoftwareMeth_Project_1/src/Library.java
@@ -6,10 +6,8 @@
 
 
 public class Library {
-
-
-    private Book[] books; // array-based implementation of the bag data structure private int numBooks; // the number of books currently in the bag
-    private int numBooks;
+    private Book[] books; // array-based implementation of the bag data structure private int numBooks;
+    private int numBooks; // the number of books currently in the bag
 
     /**
      * This is a contructor to create an initial empty bag of type Book. 
@@ -18,17 +16,15 @@ public class Library {
         books = new Book[4];
     }
 
-
     public static final int BAG_GROW_VALUE = 4;
     public static final int NOT_FOUND = -1;
 
     /**
      * This is a method to find a certain book in the bag, given and input of type Book. 
-     * @param book 
+     * @param book that is trying to be found
      * @return i, the index of the book being searched for.  
      */
     private int find(Book book) { // helper method to find a book in the bag
-
         for (int i = 0; i < books.length; i++){
             if (book == books[i]){
                 return i;
@@ -37,14 +33,13 @@ public class Library {
         return NOT_FOUND; //book not found
     }
 
-
     /**
      * This is a method to grow the bag by a given value of 4, anytime the bag gets overfilled by books. 
      * It creates a new bag and copies the old bag into this new one. 
      */
     private void grow() {  // helper method to grow the capacity by 4
-
         Book[] temp = new Book[books.length + BAG_GROW_VALUE]; //temp to hold all books before copying into new bag
+
         for (int i = 0; i < numBooks; i++){
             temp[i] = books[i];
         }
@@ -53,7 +48,7 @@ public class Library {
 
     /**
      * This method adds a given inputted book at the end of the bag.
-     * @param book 
+     * @param book being added into the bag
      */
     public void add(Book book) { //adds book to end of array. If full, calls grow()
         if (books[books.length-1]!= null){
@@ -68,12 +63,11 @@ public class Library {
         }
     }
 
-
     /**
      * This method removes a given book from the bag by setting that respective index to null.
      * Additionally, it shifts books further in the bag to the left following the removal. 
      * This readjustment is to make sure there is no empty spot in the middle of the bag. 
-     * @param book
+     * @param book being removed from the bag
      * @return true if the removal was successful, or false if the removal was unsuccessful. 
      */
     public boolean remove(Book book) { //also shifts and readjusts all interior elements after removal
@@ -92,22 +86,19 @@ public class Library {
             }
             books[j] = books[j + 1]; //set each element to value of next to preserve order of books
         }
-
         return false; //also displays "Unable to remove, the library does not have this book."
     }
 
     /**
      * This method checks out a book by setting the book's "checkedOut" value to true.
-     * @param book
+     * @param book being checked out
      * @return true if check out was successful, or false if it was unsuccessful. 
      */
     public boolean checkOut(Book book) {
-
         int index_to_find = find(book);
 
         if (index_to_find == NOT_FOUND) //if book is not owned return false
             return false;
-
         if (!books[index_to_find].getCheckedout()){ //if not checked out, set checkedOut to true
             books[index_to_find].setCheckedOut(true);
             return true; // return true if succesfully checked out
@@ -115,23 +106,19 @@ public class Library {
         else if (books[index_to_find].getCheckedout()){ //if already checked out, return false
             return false; // return false if already checked out
         }
-
         return false; //return false if checkout failed for any other reason
     }
 
-
     /**
      * This method returns a checked out book by setting its checkedOut() value to false. 
-     * @param book
+     * @param book being returned
      * @return true if return was successful, or false if return was unsuccessful. 
      */
     public boolean returns(Book book) {
-
         int index_to_find = find(book);
 
         if (index_to_find == NOT_FOUND) //if book is not owned, return false
             return false;
-
         if (books[index_to_find].getCheckedout()){ //if it is indeed checked out, check in and return true
             books[index_to_find].setCheckedOut(false);
             return true; // return true if succesfully returned
@@ -139,25 +126,18 @@ public class Library {
         else if (!books[index_to_find].getCheckedout()){ //if book is not checked out, return false
             return false; // return false if not checked out
         }
-
         return false; //return false if book return failed for any other reason
-
-
     }
-
-
 
     /**
      * This method prints out all books in the bag in a sequence. 
      */
     public void print() {  //print the list of books in the bag
-
         for(int i = 0; i < books.length; i++){
             if (books[i]!= null) {
                 System.out.println(books[i].toString());
             }
         }
-
     }
 
     /**
@@ -165,50 +145,38 @@ public class Library {
      * It uses a selection sort algorithm to sort all the books in the bag. 
      */
     public void printByDate() { //print the list of books by datePublished (ascending)
-
-
-
         //Using a simple selection sort algorithm to sort books by date in ascending order
-        for (int i = 0; i < books.length-1; i++) //keep track of sorted part of bag
-        {
-
+        for (int i = 0; i < books.length-1; i++) {   //keep track of sorted part of bag
             int min_idx = i;
             for (int j = i + 1; j < books.length; j++) {
-
-                if ( books[j] == null || books[min_idx] == null) //if space in bag is empty, ignore and continue
+                if ( books[j] == null
+                        || books[min_idx] == null) //if space in bag is empty, ignore and continue
                     continue;
-
-                if (books[j].getDatePublished().getYear() < books[min_idx].getDatePublished().getYear()){ //if year of index j is less than that of min_idx
+                if (books[j].getDatePublished().getYear()
+                        < books[min_idx].getDatePublished().getYear()){ //if year of index j is less than that of min_idx
                     min_idx = j;
-
                 }
-                if (books[j].getDatePublished().getYear() == books[min_idx].getDatePublished().getYear()){ //if year of index j is equal to that of min_idx, check month
-
-                    if ( books[j].getDatePublished().getMonth() < books[min_idx].getDatePublished().getMonth() ){ //continue to compare month values
+                if (books[j].getDatePublished().getYear()
+                        == books[min_idx].getDatePublished().getYear()){ //if year of index j is equal to that of min_idx, check month
+                    if ( books[j].getDatePublished().getMonth()
+                            < books[min_idx].getDatePublished().getMonth()){ //continue to compare month values
                         min_idx = j;
                     }
-
-                    if ( books[j].getDatePublished().getMonth() == books[min_idx].getDatePublished().getMonth()){ //if months are equal, compare day
-
-                        if ( books[j].getDatePublished().getDay() < books[min_idx].getDatePublished().getDay()){ //compare day values
+                    if ( books[j].getDatePublished().getMonth()
+                            == books[min_idx].getDatePublished().getMonth()){ //if months are equal, compare day
+                        if ( books[j].getDatePublished().getDay()
+                                < books[min_idx].getDatePublished().getDay()){ //compare day values
                             min_idx = j;
                         }
-
                     }
                 }
-
-
             }
-
             //swapping the minimum element into the sorted region to complete iteration
             Book temp = books[min_idx];
             books[min_idx] = books[i];
             books[i] = temp;
         }
-
-        print(); //print the sorted bag 
-
-
+        print(); //print the sorted bag
     }
 
     /**
@@ -216,34 +184,27 @@ public class Library {
      * It uses a selection sort algorithm to sort all the books in the bag. 
      */
     public void printByNumber() { //print the list of books by number (ascending) }
-
-
         //Using a simple selection sort algorithm to sort books by serial number in ascending order
         for (int i = 0; i < books.length - 1; i++) //keep track of sorted part of bag
         {
-
             int min_idx = i;
             for (int j = i + 1; j < books.length; j++) {
-
                 if (books[j] == null || books[min_idx] == null) //if space in bag is empty, ignore and continue
                     continue;
-
                 if (Integer.parseInt(books[j].getSerial()) < Integer.parseInt(books[min_idx].getSerial())) //find smallest SN in unsorted region of bag
                     min_idx = j;
             }
-
             //swapping the minimum element into the sorted region to complete iteration
             Book temp = books[i];
             books[i] = books[min_idx];
             books[min_idx] = temp;
         }
-
         print(); //print the sorted bag
     }
 
     /**
      * This method is used to search for a book in the bag with an inputted serial/book number for convenience if needed. 
-     * @param number
+     * @param number (serial number) of the book
      * @return this returns the book object being searched for, or returns null if not found.
      */
     public Book search(int number){
