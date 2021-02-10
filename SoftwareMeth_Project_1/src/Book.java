330a3175e3515f498fd8c845c271610212b95e13
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-09 22:15:55 -0500

    Update Book.java

--- SoftwareMeth_Project_1/src/Book.java
+++ SoftwareMeth_Project_1/src/Book.java
@@ -1,15 +1,26 @@
-
+/**
+ * This class defines type Book, which encapsulates the data fields and methods of a book.
+ * Each book has private variables number, name, checkedOut, and datePublished
+ * Books are uniquely identified by their number
+ * @author Anuraj Dubey, Chenghao Lin
+ */
 public class Book {
     private String number; //a 5-digit serial number unique to the book
     private String name;
     private boolean checkedOut;
     private Date datePublished;
 
-    public Book(String number, String name, Date datePublished){
-        this.number=number;
-        this.name=name;
-        this.datePublished=datePublished;
-        this.checkedOut=false;
+    /**
+     * Constructor for Book object which will always have a serial number, name, and publishing date, Books are not checked out by default.
+     * @param number The unique Serial Number of the book
+     * @param name   The title of the book
+     * @param datePublished The publishing date of the book in MM/DD/YYYY format
+     */
+    public Book(String number, String name, Date datePublished) {
+        this.number =  number;
+        this.name = name;
+        this.datePublished = datePublished;
+        this.checkedOut = false;
     }
 
 
