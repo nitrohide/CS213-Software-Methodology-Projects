76c1aa3cbe10e44bf1ffe42f3bc3e3858e502a0e
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-22 16:14:11 -0500

    Update PayrollProcessing.java

--- 214-project-2/src/PayrollProcessing.java
+++ 214-project-2/src/PayrollProcessing.java
@@ -18,6 +18,7 @@ public class PayrollProcessing {
         System.out.println("Payroll Processing starts.");
         boolean payrollStatus = true;
         Scanner scanner = new Scanner(System.in);
+
         while (payrollStatus && scanner.hasNext()){
             String nextLine = scanner.nextLine();
             if (nextLine.equals("")){
@@ -38,50 +39,191 @@ public class PayrollProcessing {
 
             switch (command) {
                 case "Q":
-                    if (numArgs == DISPLAY_ARGS)  {
+                    if (numArgs == DISPLAY_ARGS) {
                         payrollStatus = false;
                         break;
-                    }
-                    else{
-                        System.out.println( "Invalid command!");
+                    } else {
+                        System.out.println("Invalid command!");
                     }
 
-                case "AP":
-                    if (numArgs == NONMANAGEMENT_ARGS){
+                case "AP":    //add a part-time employee
+                    if (numArgs == NONMANAGEMENT_ARGS) {
                         name = tokens.nextToken();
                         department = tokens.nextToken();
-                        dateHired = tokens.nextToken();
-                        salary = tokens.nextToken();
-                        Date date = new Date(dateHired);
-                        double hourlyRate = Double.parseDouble(salary);
+                        Date date = new Date(tokens.nextToken());
+                        double hourlyRate = Double.parseDouble(tokens.nextToken());
 
-                        if (!isValidDept(department)){
+                        if (!isValidDept(department)) {
                             System.out.println("'" + department + "'" + " is not a valid department code.");
-                        }
-                        else if (hourlyRate < MINIMUM_SALARY){
+                        } else if (hourlyRate < MINIMUM_SALARY) {
                             System.out.println("Pay rate cannot be negative.");
-                        }
-                        else if (!date.isValid()){
+                        } else if (!date.isValid()) {
                             System.out.println("Invalid Date!");
-                        }
-                        else {
+                        } else {
                             Parttime newParttime = new Parttime(name, department, date, hourlyRate);
                             Company.add(newParttime);
                             System.out.println("Employee added.");
                         }
-                    }
-                    else{
+                    } else {
                         System.out.println("Invalid Command!");
                     }
                     break;
-                case "AF":
 
+                case "AF":  //add a full-time employee
+                    if (numArgs == NONMANAGEMENT_ARGS) {
+                        name = tokens.nextToken();
+                        department = tokens.nextToken();
+                        Date date = new Date(tokens.nextToken());
+                        double yearlySalary = Double.parseDouble(tokens.nextToken());
 
+                        if (!isValidDept(department)) {
+                            System.out.println("'" + department + "'" + " is not a valid department code.");
+                        } else if (yearlySalary < MINIMUM_SALARY) {
+                            System.out.println("Salary cannot be negative.");
+                        } else if (!date.isValid()) {
+                            System.out.println("Invalid Date!");
+                        } else {
+                            Fulltime newFulltime = new Fulltime(name, department, date, yearlySalary);
+                            Company.add(newFulltime);
+                            System.out.println("Employee added.");
+                        }
+                    } else {
+                        System.out.println("Invalid Command!");
+                    }
+                    break;
+
+                case "AM":   //add a management employee
+                    if (numArgs == MANAGEMENT_ARGS) {
+                        name = tokens.nextToken();
+                        department = tokens.nextToken();
+                        Date date = new Date(tokens.nextToken());
+                        double yearlySalary = Double.parseDouble(tokens.nextToken());
+                        role = tokens.nextToken();
+
+                        if (!isValidDept(department)) {
+                            System.out.println("'" + department + "'" + " is not a valid department code.");
+                        } else if (yearlySalary < MINIMUM_SALARY) {
+                            System.out.println("Salary cannot be negative.");
+                        } else if (!date.isValid()) {
+                            System.out.println("Invalid Date!");
+                        } else if (!isValidManager(role)) {
+                            System.out.println("Invalid management code.");
+                        } else {
+                            Fulltime newFulltime = new Fulltime(name, department, date, yearlySalary);
+                            Company.add(newFulltime);
+                            System.out.println("Employee added.");
+                        }
+                    } else {
+                        System.out.println("Invalid Command!");
+                    }
+
+                case "R":
+                    if (numArgs == REMOVE_ARGS) {
+                        name = tokens.nextToken();
+                        department = tokens.nextToken();
+                        Date date = new Date(tokens.nextToken());
+
+                        Employee removeEmployee = new Employee(name, department, date);
+                        boolean isRemoved = Company.remove(removeEmployee);
+
+                        if (!isRemoved) {
+                            System.out.println("Employee doesn't exist.");
+                        } else {
+                            System.out.println("Employee removed.");
+                        }
+                    } else {
+                        System.out.println("Invalid command!");
+                    }
+                    break;
+                case "C":
+                    if (numArgs == DISPLAY_ARGS) {
+                        System.out.println("Calculation of employee payments is done");
+                        Company.processPayments();
+                    } else {
+                        System.out.println("Invalid command!");
+                    }
+                    break;
+                case "S":
+                    if (numArgs == NONMANAGEMENT_ARGS) {
+                        name = tokens.nextToken();
+                        department = tokens.nextToken();
+                        Date date = new Date(tokens.nextToken());
+                        int Hours = Integer.parseInt(tokens.nextToken());
+
+                        if (Hours < MINIMUM_HOURS) {
+                            System.out.println("Working hours cannot be negative.");
+                        }
+                        if (Hours > MAXIMUM_HOURS) {
+                            System.out.println("Invalid Hours: over 100.");
+                        } else {
+                            Employee setEmployee = Company.search(name, department, date);
+                            if (setEmployee == null) {
+                                System.out.println("Employee doesn't exist.");
+                            } else if (!Company.setHours(setEmployee)) {
+                                System.out.println("Employee is not part-time");
+                            } else {
+                                Company.setHours(setEmployee); //DOES THIS SET HOURS?
+                                System.out.println("Working hours set.");
+                            }
+                        }
+                    } else {
+                        System.out.println("Invalid command!");
+                    }
+                    break;
+
+                case "PA":
+                    if (numArgs == DISPLAY_ARGS) {
+                        if (Company.getNumEmployee() == 0) {
+                            System.out.println("Employee database is empty.");
+                        }
+                        else{
+                            System.out.println("--Printing earning statements for all employees--");
+                            Company.print();
+                        }
+                    }
+                    else{
+                        System.out.println("Invalid command!");
+                    }
+                    break;
+
+                case "PH":
+                    if (numArgs == DISPLAY_ARGS) {
+                        if (Company.getNumEmployee() == 0) {
+                            System.out.println("Employee database is empty.");
+                        }
+                        else{
+                            System.out.println("--Printing earning statements for all employees--");
+                            Company.printByDate();
+                        }
+                    }
+                    else{
+                        System.out.println("Invalid command!");
+                    }
+                    break;
+
+                case "PD":
+                    if (numArgs == DISPLAY_ARGS) {
+                        if (Company.getNumEmployee() == 0) {
+                            System.out.println("Employee database is empty.");
+                        }
+                        else{
+                            System.out.println("--Printing earning statements for all employees--");
+                            Company.printByDepartment();
+                        }
+                    }
+                    else{
+                        System.out.println("Invalid command!");
+                    }
+                    break;
+
+                case "":
+                    break;
+
+                default:
+                    System.out.println("Command " + command + " not supported!");
             }
-
-
         }
-
+        System.out.println("Payroll Processing completed.");
     }
 
     public boolean isValidDept(String department){
