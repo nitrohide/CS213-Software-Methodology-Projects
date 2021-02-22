83ff38cc306656c82b0e3f636111f04a0dae42f8
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-22 13:26:30 -0500

    Update Profile.java

--- 214-project-2/src/Profile.java
+++ 214-project-2/src/Profile.java
@@ -2,6 +2,14 @@ public class Profile {
     private String name; //employee’s name in the form “lastname,firstname”
     private String department; //department code: CS, ECE, IT
     private Date dateHired;
+
+    public Profile(String name, String department, Date dateHired){
+        this.name = name;
+        this.department = department;
+        this.dateHired = dateHired;
+    }
+
+
     @Override
     public String toString() { }
     @Override
