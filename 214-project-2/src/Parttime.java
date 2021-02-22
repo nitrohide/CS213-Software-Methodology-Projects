5b4ce5b5000b827ead1eeaf6a5376d15c5b2f73d
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-22 13:27:03 -0500

    Update Parttime.java

--- 214-project-2/src/Parttime.java
+++ 214-project-2/src/Parttime.java
@@ -1,2 +1,14 @@
-public class Parttime {
+public class Parttime extends Employee{
+    final int MAXIMUM_HOURS = 100;
+    final int WEEKLY_MAX = 40;
+    final double OVERTIME_RATE = 1.5;
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public Parttime(String name, String department, Date dateHired, double hourlyRate){
+        super(name,department,dateHired);
+        this.hourlyRate = hourlyRate;
+    }
+
 }
