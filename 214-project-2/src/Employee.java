43ec69a2a2009828ddf63ee1b523aec61d304d8f
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-22 13:26:45 -0500

    Update Employee.java

--- 214-project-2/src/Employee.java
+++ 214-project-2/src/Employee.java
@@ -1,2 +1,8 @@
 public class Employee {
+    private Profile profile;
+
+    public Employee(String name, String department, Date dateHired){
+        profile = new Profile(name,department,dateHired);
+
+    }
 }
