2eeec6d70a2daf56fcb7112cd82e2951f1d93dbd
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-22 13:34:36 -0500

    Update Fulltime.java

--- 214-project-2/src/Fulltime.java
+++ 214-project-2/src/Fulltime.java
@@ -1,2 +1,8 @@
-public class Fulltime {
+public class Fulltime extends Employee{
+    private double yearlySalary;
+
+    public Fulltime(String name, String department, Date dateHired, double yearlySalary){
+        super(name,department,dateHired);
+        this.yearlySalary = yearlySalary;
+    }
 }
