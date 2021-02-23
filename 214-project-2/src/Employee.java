980587f4c8c125daa36270af00be86147c4b59dc
AnurajDubey <anurajd2000@gmail.com>
2021-02-23 14:38:24 -0500

    Update Employee.java

--- 214-project-2/src/Employee.java
+++ 214-project-2/src/Employee.java
@@ -1,12 +1,26 @@
 public class Employee {
+    
     private Profile profile;
+    private int hoursWorked;
 
     public Employee(String name, String department, Date dateHired){
         profile = new Profile(name,department,dateHired);
+        
 
     }
-    @Override
-    public String toString() { }
-    @Override
-    public boolean equals(Object obj) { } //compare name, department and dateHired
+
+    public Profile getProfile(){
+        return profile; 
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked){
+        this.hoursWorked = hoursWorked; 
+    }
+
+
+    
 }
