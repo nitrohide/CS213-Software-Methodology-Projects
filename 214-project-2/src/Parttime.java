c7d33e56a2160010bc8b414306ce17341835cb09
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-22 22:26:42 -0500

    Update Parttime.java

--- 214-project-2/src/Parttime.java
+++ 214-project-2/src/Parttime.java
@@ -11,4 +11,12 @@ public class Parttime extends Employee{
         this.hourlyRate = hourlyRate;
     }
 
+    @Override
+    public void calculatePayment() { }
+
+    @Override
+    public String toString() { }
+    @Override
+    public boolean equals(Object obj) { } //compare name, department and dateHired
+
 }
