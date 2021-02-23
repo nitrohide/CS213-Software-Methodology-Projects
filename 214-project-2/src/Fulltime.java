158ec59a157375b910e0f44157beb9637065c5cf
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-23 15:21:44 -0500

    Update Fulltime.java

--- 214-project-2/src/Fulltime.java
+++ 214-project-2/src/Fulltime.java
@@ -1,5 +1,6 @@
 public class Fulltime extends Employee{
     private double yearlySalary;
+    public static final int PAYPERIODS = 26;
 
     public Fulltime(String name, String department, Date dateHired, double yearlySalary){
         super(name,department,dateHired);
@@ -7,10 +8,17 @@ public class Fulltime extends Employee{
     }
 
     @Override
-    public void calculatePayment() { }
+    public void calculatePayment() {
+        double payment = this.yearlySalary / PAYPERIODS;
+        super.setPayment(payment);
+    }
 
     @Override
     public String toString() { }
     @Override
     public boolean equals(Object obj) { } //compare name, department and dateHired
+
+    public double getSalary(){
+        return yearlySalary;
+    }
 }
