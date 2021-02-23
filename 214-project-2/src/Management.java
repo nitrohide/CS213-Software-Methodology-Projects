dd862701dbf26be9fd79f2fc605386ae36654620
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-02-23 15:21:31 -0500

    Update Management.java

--- 214-project-2/src/Management.java
+++ 214-project-2/src/Management.java
@@ -1,2 +1,39 @@
-public class Management {
+public class Management extends Fulltime{
+    int role;
+    String role_name;
+    final static int MANAGER = 1;
+    final static double MANAGER_COMPENSATION = 5000;
+    final static int DEPARTMENT_HEAD = 2;
+    final static double DEPARTMENT_HEAD_COMPENSATION = 9500;
+    final static int DIRECTOR= 3;
+    final static double DIRECTOR_COMPENSATION = 12000;
+
+    public Management(String name, String department, Date dateHired, double yearlySalary, int role){
+        super(name,department,dateHired, yearlySalary);
+        this.role = role;
+    }
+
+    @Override
+    public void calculatePayment() {
+        int compensation;
+        double payment = getSalary()/26;
+        if (this.role == MANAGER){
+            payment = MANAGER_COMPENSATION + payment;
+            role_name = "Manager";
+        }
+        else if (this.role == DEPARTMENT_HEAD){
+            payment = DEPARTMENT_HEAD_COMPENSATION + payment;
+            role_name = "Department Head";
+        }
+        else if (this.role == DIRECTOR){
+            payment = DIRECTOR_COMPENSATION + payment;
+            role_name = "Director";
+        }
+        super.setPayment(payment);
+    }
+
+    @Override
+    public String toString() { }
+    @Override
+    public boolean equals(Object obj) { } //compare name, department and dateHired
 }
