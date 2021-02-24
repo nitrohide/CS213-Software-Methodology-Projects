e09c95f1b888e1708175d741b233218e53545bbe
AnurajDubey <anurajd2000@gmail.com>
2021-02-23 21:41:03 -0500

    Update RunProject2.java

--- 214-project-2/src/RunProject2.java
+++ 214-project-2/src/RunProject2.java
@@ -1,3 +1,7 @@
+/**
+ * Driver for Project 2. Calls run() from PayrollProcessing to begin. 
+ * @author Anuraj Dubey, Chenghao Lin 
+ */
 public class RunProject2 {
     public static void main(String[] args) {
         new PayrollProcessing().run();
