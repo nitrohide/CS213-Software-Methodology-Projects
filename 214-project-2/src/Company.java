7f429dbebf8b6cb9c612200cd30baee5524c304f
AnurajDubey <anurajd2000@gmail.com>
2021-02-23 21:24:00 -0500

    Update Company.java

--- 214-project-2/src/Company.java
+++ 214-project-2/src/Company.java
@@ -1,7 +1,16 @@
+/**
+ * The Company class is responsible for creating and maintaining a container, or array, of type Employee. 
+ * It holds multiple methods that revolve around maintaining and manipulating this array in order to serve as an effective Company
+ * payment management sytem.
+ * @author Anuraj Dubey, Chenghao Lin 
+ */
 public class Company {
     private Employee[] emplist;
     private int numEmployee;
 
+    /**
+     * This is a constructor to create an initial container list of Employees
+     */
     public Company(){//constructor to create empty container for employee list
         emplist = new Employee[BAG_INITIAL_VALUE];
     }
@@ -10,6 +19,11 @@ public class Company {
     public static final int BAG_GROW_VALUE = 4;
     public static final int NOT_FOUND = -1;
 
+    /**
+     * This is a method to find a certain employee in the list, given and input of type Employee. 
+     * @param employee The employee that is trying to be found. 
+     * @return i, the index of the book being searched for.  
+     */
     private int find(Employee employee) {
         for (int i = 0; i < numEmployee; i++){
             if (employee.getProfile().equals(emplist[i].getProfile())){
@@ -19,6 +33,10 @@ public class Company {
         return NOT_FOUND; //employee not found
     }
 
+    /**
+     * This is a method to grow the list by a given value of 4, anytime the list gets overfilled by Employees. 
+     * It creates a new list and copies the old list into this new one. 
+     */ 
     private void grow() {
 
         Employee[] temp = new Employee[emplist.length + BAG_GROW_VALUE]; //temp to hold all books before copying into new bag
@@ -30,6 +48,11 @@ public class Company {
 
     }
 
+    /**
+     * This method adds a given inputted employee at the end of the list.
+     * @param employee The employee being added into the bag
+     * @return Returns true if add was successful, and false otherwise. 
+     */
     public boolean add(Employee employee) {  //check the profile before adding
 
         if (emplist[emplist.length-1]!= null){
@@ -46,6 +69,13 @@ public class Company {
         return false;
     }
 
+    /**
+     * This method removes a given Employee from the list by setting that respective index to null.
+     * Additionally, it shifts employees further in the bag to the left following the removal. 
+     * This readjustment is to make sure there is no empty spot in the middle of the bag. 
+     * @param employee The employee being removed from the list
+     * @return true if the removal was successful, or false if the removal was unsuccessful. 
+     */
     public boolean remove(Employee employee) {
 
         int index_to_remove = find(employee);
@@ -67,6 +97,12 @@ public class Company {
 
     }
 
+    /**
+     * This method takes an employee and searches for the equivalent employee in the list.
+     * If the employee is found, AND they are indeed a part-time worker, the hours they worked during the pay period will be updated.
+     * @param employee An equivalent employee object which serves to help update the real employee object's hours worked. 
+     * @return Returns True if the working hours update was successful and false otherwise. 
+     */
     public boolean setHours(Employee employee) {  //set working hours for a part time
 
         for(int i = 0; i < emplist.length; i++){
@@ -83,6 +119,9 @@ public class Company {
 
     }
 
+    /**
+     * This method just kickstarts the payment processing by sequentially calling calculatePayment() on every Employee in the list. 
+     */
     public void processPayments() { //process payments for all employees
 
         for(int i = 0; i < numEmployee; i++){
@@ -91,6 +130,9 @@ public class Company {
 
     }
 
+    /**
+     * This method prints out all employees in the list in a sequence. 
+     */
     public void print() {  //print earning statements for all employees
 
         for(int i = 0; i < emplist.length; i++){
@@ -100,6 +142,9 @@ public class Company {
         }
     }
 
+    /**
+     * This method prints out all earning statments grouped by department. 
+     */
     public void printByDepartment() { //print earning statements by department
 
         for( int i = 0; i < numEmployee; i++){
@@ -122,6 +167,9 @@ public class Company {
 
     }
 
+    /**
+     * This method prints all earning statements sorted by the date each employee was hired. 
+     */
     public void printByDate() {  //print earning statements by date hired
 
         //Using a simple selection sort algorithm to sort employees by date in ascending order
@@ -159,6 +207,13 @@ public class Company {
 
     }
 
+    /**
+     * This method is used to search for an Employee in the list with an inputted name, department, and date hired, if needed for convenience.
+     * @param name The name of the employee.
+     * @param department The department the employee works in.
+     * @param dateHired The date the employee was hired. 
+     * @return this returns the employee object itself that needed to be found. 
+     */
     public Employee search(String name, String department, Date dateHired){
         Employee tempEmployee = new Employee(name,department,dateHired);
         int location = find(tempEmployee);
@@ -170,6 +225,10 @@ public class Company {
         }
     }
 
+    /**
+     * Method to get the number of employees.
+     * @return
+     */
     public int getNumEmployee(){
         return numEmployee;
     }
