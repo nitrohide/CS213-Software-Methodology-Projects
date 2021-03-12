6bde31b68dc037ff63e5858423aa2d73705862b0
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-03-12 01:21:57 -0500

    Add files via upload

---
+++ Softwaremeth_project_3/src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        try {
+            BorderPane root = FXMLLoader.load(getClass().getResource("view.fxml"));
+            Scene scene = new Scene(root, 500, 600);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Payroll Processing");
+            primaryStage.show();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
