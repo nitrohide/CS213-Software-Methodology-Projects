3443fcc3067b50b6f456d840206ca589285e7c5c
nitrohide <46761439+nitrohide@users.noreply.github.com>
2021-03-12 04:07:16 -0500

    Update Controller.java

--- Softwaremeth_project_3/src/sample/Controller.java
+++ Softwaremeth_project_3/src/sample/Controller.java
@@ -1,23 +1,79 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ToggleGroup;
 
 public class Controller {
+
+    @FXML
+    private RadioButton csButton;
+
+    @FXML
+    private ToggleGroup departmentGroup;
+
+    @FXML
+    private RadioButton itButton;
+
+    @FXML
+    private RadioButton eceButton;
+
+    @FXML
+    private DatePicker dateInput;
+
+    @FXML
+    private RadioButton fulltimeButton;
+
+    @FXML
+    private ToggleGroup employeeGroup;
+
+    @FXML
+    private RadioButton parttimeButton;
+
+    @FXML
+    private RadioButton managementButton;
+
+    @FXML
+    private TextField salaryInput;
+
+    @FXML
+    private TextField hoursInput;
+
+    @FXML
+    private TextField rateInput;
+
+    @FXML
+    private RadioButton managerButton;
+
+    @FXML
+    private ToggleGroup roleGroup;
+
+    @FXML
+    private RadioButton deptHeadButton;
+
+    @FXML
+    private RadioButton directorButton;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button setHoursButton;
+
+    @FXML
+    private TextArea textArea;
 
 }
